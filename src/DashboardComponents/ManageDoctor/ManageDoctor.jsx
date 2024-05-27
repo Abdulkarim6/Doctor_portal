5187031e84edc57ce026cdc931c65a4edb27bf44
Abdulkarim6 <mdhossainjwel687@gmail.com>
2024-05-27 11:29:53 +0600

    delete my-project folder

--- src/DashboardComponents/ManageDoctor/ManageDoctor.jsx
+++ src/DashboardComponents/ManageDoctor/ManageDoctor.jsx
@@ -19,6 +19,7 @@ const ManageDoctor = () => {
                 }
             })
             const data = await res.json();
+            console.log(data);
             return data;
 
         }
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -18,33 +18,63 @@ import PrivateRoute from "./PrivateRoute";
 import AppointmentPayment from "../DashboardComponents/AppointmentPayment/AppointmentPayment";
 
 const router = createBrowserRouter([
-    {
-        path: '/', element: <MainLayout></MainLayout>,
-        children: [
-            { path: '/', element: <Home></Home> },
-            { path: '/appointment', element: <Appointment></Appointment> },
-            { path: '/about', element: <DentalCare></DentalCare> },
-            { path: '/testimonials', element: <Testimonials></Testimonials> },
-            { path: '/contuct', element: <Contuct></Contuct> },
-            { path: '/logIn', element: <SignIn></SignIn> },
-            { path: '/forgetPassword', element: <ForgetPassword></ForgetPassword> },
-            { path: '/signup', element: <SignUp></SignUp> }
-        ]
-    },
-    {
-        path: '/dashboard', element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
-        children: [
-
-            { path: '/dashboard', element: <MyAppointment></MyAppointment> },
-            { path: '/dashboard/Users', element: <AdminRoute><Users></Users></AdminRoute> },
-            { path: '/dashboard/addDoctor', element: <AdminRoute><AddDoctor></AddDoctor></AdminRoute> },
-            { path: '/dashboard/manageDoctor', element: <AdminRoute><ManageDoctor></ManageDoctor></AdminRoute> },
-            {
-                path: '/dashboard/payment/:id', element:<AppointmentPayment></AppointmentPayment>,
-                loader: ({ params }) => fetch(`https://hospital-server-code.vercel.app/appointment/${params.id}`)
-            },
-        ]
-    }
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "/appointment", element: <Appointment></Appointment> },
+      { path: "/about", element: <DentalCare></DentalCare> },
+      { path: "/testimonials", element: <Testimonials></Testimonials> },
+      { path: "/contuct", element: <Contuct></Contuct> },
+      { path: "/logIn", element: <SignIn></SignIn> },
+      { path: "/forgetPassword", element: <ForgetPassword></ForgetPassword> },
+      { path: "/signup", element: <SignUp></SignUp> },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout></DashboardLayout>
+      </PrivateRoute>
+    ),
+    children: [
+      { path: "/dashboard", element: <MyAppointment></MyAppointment> },
+      {
+        path: "/dashboard/Users",
+        element: (
+          <AdminRoute>
+            <Users></Users>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/addDoctor",
+        element: (
+          <AdminRoute>
+            <AddDoctor></AddDoctor>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/manageDoctor",
+        element: (
+          <AdminRoute>
+            <ManageDoctor></ManageDoctor>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/payment/:id",
+        element: <AppointmentPayment></AppointmentPayment>,
+        loader: ({ params }) =>
+          fetch(
+            `https://hospital-server-code.vercel.app/appointment/${params.id}`
+          ),
+      },
+    ],
+  },
 ]);
 
 export default router;
